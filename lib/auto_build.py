1bb934750a5ac2129cd7c05e2cf94b25ca828aff
star <linjx@chuchujie.com>
2018-07-03 20:40:21 +0800

    1

--- lib/auto_build.py
+++ lib/auto_build.py
@@ -5,9 +5,12 @@
 # QQ: 279370682
 # 2018/7/3 20:05
 
+from lib.github import GitHub
+
 
 def auto_build():
     """
         自动提交CodeBuild任务构建
     :return:
     """
+    eos_github = GitHub()
---
+++ lib/github.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+# coding=utf-8
+# 林建星
+# linjx@chuchujie.com
+# QQ: 279370682
+# 2018/7/3 20:12
+
+
+class GitHub(object):
+    """
+        github 操作类
+    """
+
+    def __init__(self, user=None, repo=None):
+        self._user = user
+        self._repo = repo
+
+    def get_latest_release_version(self):
+        """
+            获取最新发布版本号
+        :return:
+        """
+        return None
+
+
+def main():
+    """
+        main
+    :return: 
+    """
+
+
+if __name__ == '__main__':
+    main()
